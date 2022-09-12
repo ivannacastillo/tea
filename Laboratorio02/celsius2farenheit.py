3d4e368f2efd43d67d868b0ca01c07be6c902b00
Ivanna Kristel Castillo Danilov (24115) <ivanna.castillo@est.zamorano.edu>
2022-09-12 16:13:07 -0600

    lab02

---
+++ Laboratorio02/celsius2farenheit.py
@@ -0,0 +1,4 @@
+celsius = input("Cual es el valor de la temperatura en grados Celsius? ")
+gradosfarenheit = (float(celsius) * 9/5) + 32
+
+print("La temperatura en grados farenheit es: ", gradosfarenheit)
